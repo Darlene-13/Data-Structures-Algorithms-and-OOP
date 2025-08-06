bd6bc8629efa7aa9528e5fc80ace29a2a8e6373f
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-06 23:01:18 +0300

    Initial commit: Stack data structures

---
+++ stack.py
@@ -0,0 +1,32 @@
+"""
+This is a file for the stack data structure.
+
+It contains the implementation of a stack using a list.
+It includes all its basic operations like push, pop, peek, and is_empty.
+Key operation is to always check if the stack is empty first before popping or peeking.
+"""
+
+class Stack:
+    def __init__(self):
+        # Initialize and empty stack.
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        if self._is_empty():
+            raise IndexError("pop from empty stack")
+        return self.items.pop()
+    
+    def peek(self):
+        if self._is_empty():
+            raise IndexError("peek from empty stack")
+        return self.items[-1]
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+
+    def size(self):
+        return len(self.items)
